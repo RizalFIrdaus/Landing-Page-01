342af12997ae22ba1c84bf0d71bcda4a6d0f14d5
Muhammad Rizal Firdaus <rizal.firdaus117@gmail.com>
2020-05-31 18:00:48 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// JavaScript
+ScrollReveal().reveal('.head', {
+    delay: 500,
+    origin: 'left',
+    distance: '50 px'
+});
+ScrollReveal().reveal('#header .content h3', {
+    delay: 500,
+    origin: 'top',
+    distance: '50 px'
+});
+ScrollReveal().reveal('#header .content p', {
+    delay: 500,
+    origin: 'bottom',
+    distance: '50 px'
+});
+
+ScrollReveal().reveal('#services .box', {
+    delay: 1000,
+    origin: 'left',
+    distance: '50 px'
+});
+ScrollReveal().reveal('.text', {
+    delay: 800,
+    origin: 'left',
+    distance: '50 px'
+});
+ScrollReveal().reveal('#technology .imgBx', {
+    delay: 1000,
+    origin: 'right',
+    distance: '50 px'
+});
+ScrollReveal().reveal('#technology .imgBx', {
+    delay: 1000,
+    origin: 'right',
+    distance: '50 px'
+});
+ScrollReveal().reveal('#clients .imgClient li', {
+    delay: 1000,
+    origin: 'bottom',
+    distance: '50 px'
+});
+ScrollReveal().reveal('#testimonial .testiText', {
+    delay: 1000,
+    origin: 'bottom',
+    distance: '50 px'
+});
+ScrollReveal().reveal('#form form', {
+    delay: 1000,
+    origin: 'bottom',
+    opacity: 0
+});
